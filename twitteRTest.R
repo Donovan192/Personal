4442da619e2fbf56afe4a401da4482f83b3985aa
Donovan Eyre <donovan@codaventures.com>
2016-12-05 21:23:25 -0600

    Update to Twitter script including a script pulled from the tutorial that graphs tweet sources - adapted by me to graph for entered user

--- twitteRTest.R
+++ twitteRTest.R
@@ -23,3 +23,22 @@ setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessTokenSecret)
 
 #Test pulling latest 10 tweet from Obama (@POTUS)
 userTimeline("POTUS", 10)
+
+# Tweet source chart from tutorial - adapted for user instead of trend
+library(ggplot2)
+# Get David's timeline (API can pull from max last two weeks I think)
+momtweets <- userTimeline('realDonaldTrump', n = 100)
+sources <- sapply(momtweets, function(x) x$getStatusSource())
+sources <- gsub("</a>", "", sources)
+sources <- strsplit(sources, ">")
+sources <- sapply(sources, function(x) ifelse(length(x) > 1, x[2], x[1]))
+source_table = table(sources)
+# Commented out these lines to stop grouping lesser used sources as "other"
+#filtered_sources = names(source_table[source_table < quantile(source_table, 0.9)])
+#sources[sources %in% filtered_sources] = "other"
+source_df = as.data.frame(sources)
+ggplot(source_df, aes(sources)) + geom_bar() + coord_flip()
+# Had to manually click plot tab to view output;
+# Apparently David only tweets from iPhone
+
+
